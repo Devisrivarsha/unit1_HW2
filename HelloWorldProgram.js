7d649ab1fcf0acb50aadcb257817e64e1d88d9e5
Devisrivarsha <156610592+Devisrivarsha@users.noreply.github.com>
2025-01-09 20:38:01 +0530

    first commit

---
+++ HelloWorldProgram.js
@@ -0,0 +1,13 @@
+// Program to display the name "Devi Sri Varsha Dodle"
+
+// Using console.log()
+// This outputs the name to the browser's console, which is useful for debugging and viewing logs.
+console.log('Devi Sri Varsha Dodle');
+
+// Using alert()
+// This shows the ID in a pop-up alert box, requiring user interaction to proceed.
+alert('f00574843');
+
+// Using document.write()
+// This directly outputs onto the web page
+document.write('Hello World!');
